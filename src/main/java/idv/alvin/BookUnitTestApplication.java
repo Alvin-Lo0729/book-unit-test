4ea74e92f6ff9be503ecb9cdc06b2ade731cb455
AlvinLo <ak47237496@gmail.com>
2023-01-11 22:06:56 +0800

    第一次上傳

---
+++ src/main/java/idv/alvin/BookUnitTestApplication.java
@@ -0,0 +1,13 @@
+package idv.alvin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookUnitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookUnitTestApplication.class, args);
+    }
+
+}
